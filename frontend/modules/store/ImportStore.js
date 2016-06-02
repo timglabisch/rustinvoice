4aa8728386237fb7d24b11644389dd77ab5d1f37
timglabisch <tim@ainfach.de>
2016-06-02 23:21:19 +0200

    improve import performance

--- frontend/modules/store/ImportStore.js
+++ frontend/modules/store/ImportStore.js
@@ -6,7 +6,7 @@ export default Reflux.createStore({
 
     init: function () {
         this.fileInfos = [];
-
+        this.concurrent_uploads = 0;
         this.listenTo(Action.require_file_upload, this.on_require_file_upload);
     },
 
@@ -17,42 +17,79 @@ export default Reflux.createStore({
         var fileInfo = {
             id: id,
             entry: fileEntry,
-            content: null,
             state: 'PRE_READ_FILE'
         };
 
         this.fileInfos.push(fileInfo);
 
-        fileEntry.file(function(file) {
-            var reader = new FileReader();
-            reader.onloadend = function () {
-
-                this.update(id, {
-                    content: reader.result,
-                    state: 'PRE_UPLOAD_FILE'
-                });
-
-                this.uploadFile(fileInfo);
-
-                this.trigger();
-            }.bind(this);
-            reader.readAsDataURL(file);
-        }.bind(this));
+        this.handle_try_work_on_queue();
     },
 
-    uploadFile: function(fileInfo) {
+    handle_try_work_on_queue : function() {
+        if (this.concurrent_uploads >= 5) {
+            return;
+        }
+
+        console.log(this.concurrent_uploads);
+
+        for (var x of this.fileInfos) {
+
+            if (x.state != "PRE_READ_FILE") {
+                continue;
+            }
+
+            this.update(x.id, {
+                state: 'PRE_UPLOAD_FILE'
+            });
+
+            this.concurrent_uploads++;
+            x.entry.file(
+                function(file) {
+                    var reader = new FileReader();
+                    reader.onloadend = function () {
+
+                        this.uploadFile(x, reader.result);
+
+                        this.trigger();
+                    }.bind(this);
+                    reader.readAsDataURL(file);
+                }.bind(this),
+                function() {
+                    this.update(x.id, {
+                        state: 'PRE_READ_FILE'
+                    });
+                    this.concurrent_uploads--;
+                }.bind(this)
+            );
+
+            if (this.concurrent_uploads >= 5) {
+                console.log("WAIT....");
+                return;
+            }
+        }
+    },
+
+    uploadFile: function(fileInfo, data) {
+
         var xhr = new XMLHttpRequest();
         xhr.open("POST", 'http://127.0.0.1:6767/import/upload', true);
         xhr.onreadystatechange = function() {
 
+            if (xhr.readyState != 4) {
+                return;
+            }
+
+            this.concurrent_uploads--;
             this.update(fileInfo.id, {
-                state: 'UPLOADED_WITH_' + xhr.readyState + '_' + xhr.status
+                state: 'UPLOADED'
             });
 
             this.trigger();
 
+            this.handle_try_work_on_queue();
+
         }.bind(this);
-        xhr.send(fileInfo.content.split(',')[1]);
+        xhr.send(data.split(',')[1]);
     },
 
     update: function(id, data) {
@@ -67,7 +104,9 @@ export default Reflux.createStore({
     },
 
     getFileInfos: function() {
-        return this.fileInfos;
+        return this.fileInfos.filter(function(v) {
+            return v.state != "PRE_READ_FILE" && v.state != "UPLOADED";
+        });
     },
 
     isLoading: function () {
